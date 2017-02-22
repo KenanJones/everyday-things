0f5c03ff4e496c63d0d24c6cbf38b8c5eededb69
Anthony <anthony.kulis@gmail.com>
2017-02-22 10:10:49 -0600

    refactors sodabottle to extend bottle

--- edu/jalc/everydaythings/bottle/SodaBottle.java
+++ edu/jalc/everydaythings/bottle/SodaBottle.java
@@ -2,23 +2,20 @@ package jalc.everydaythings.bottle;
 
 import jalc.everydaythings.liquid.Soda;
 
-public class SodaBottle {
-	private double capacity;
+public class SodaBottle extends Bottle {
+
 	private Soda soda;
 
 	private SodaBottle() {
-		this.capacity = 0;
+		super(0);
 		this.soda = null;
 	}
 
 	public SodaBottle(double capacity, Soda soda) {
-		this.capacity = capacity;
+		super(capacity);
 		this.soda = soda;
 	}
 
-	public double getCapacity() {
-		return capacity;
-	}
 
 	public Soda getSoda() {
 		return soda;
